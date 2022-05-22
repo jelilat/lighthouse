2352a237ef1829384c234e3ceae50e99cba5a116
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-05-22 20:41:41 +0100

    add note

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ function App() {
       </div>
       <Borrow />
       <Lend />
+      <div className="footer">
+        <h2>Note</h2>
+        <ul>
+          <li>The NFTs used by this contract can be minted <a href="https://rinkeby.etherscan.io/address/0x2bff1618c8052f90c60812b994c4febfcbd856d7#writeContract">here</a>. You need it to list an item.</li>
+          <li>The contract only accepts USDC. Test USDC can be minted <a href="https://rinkeby.etherscan.io/token/0xeb8f08a975ab53e34d8a0330e0d34de942c95926#writeContract">here</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
